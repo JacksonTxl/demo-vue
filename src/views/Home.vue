1aded727c74f5fd9b56dfebedf934487baa997ea
张飞 <172004133@qq.com>
2020-01-15 14:06:26 +0800

    feat: subject

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,30 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import HelloWorld from "@/components/HelloWorld.vue";
+import { getThisObject } from "../utils/test.ts";
 export default {
-  name: 'home',
+  name: "home",
   components: {
     HelloWorld
+  },
+  data() {
+    return {
+      getThisObject,
+      test: 12314
+    };
+  },
+  // computed: {
+  //   getThisObject
+  // },
+  mounted() {
+    this.getThisObject()
   }
-}
+};
 </script>
